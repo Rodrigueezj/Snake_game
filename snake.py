2c1329bea897e6b27faf8fe3474ab88abb1f4caa
Juan <jdbelloro@unal.edu.co>
2023-03-21 20:49:57 -0500

    Apple bug

--- snake.py
+++ snake.py
@@ -1,4 +1,6 @@
 import time
+import pygame
+from pygame import display, time, draw, QUIT, init, KEYDOWN, K_a, K_s, K_d, K_w
 from pygame import time
 import numpy as np
 import cv2
@@ -9,14 +11,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
+init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 done = False
 cols = 17
 rows = 19
 width = 600
 height = 530
-wr = width/cols
-hr = height/rows
+wr = width/rows
+hr = height/cols
 direction = 1
+screen = display.set_mode([width, height])
+display.set_caption("snake_self")
 clock = time.Clock()
 
 def get_path(food1, snake1):
@@ -54,7 +66,6 @@ def get_path(food1, snake1):
         elif current1.x > current1.camefrom.x and current1.y == current1.camefrom.y:
             dir_array1.append(1)
         current1 = current1.camefrom
-    print(dir_array1)
     for i in range(rows):
         for j in range(cols):
             grid[i][j].camefrom = []
@@ -62,7 +73,6 @@ def get_path(food1, snake1):
             grid[i][j].h = 0
             grid[i][j].g = 0
     return dir_array1
-
 
 def get_head():
     # Capture screenshot of entire browser window
@@ -92,7 +102,11 @@ def get_head():
     eye2_center = eye_centers[1]
     distance = np.sqrt((eye2_center[0] - eye1_center[0])**2 + (eye2_center[1] - eye1_center[1])**2)
     head_center = int((eye1_center[0] + eye2_center[0])/2), int((eye1_center[1] + eye2_center[1])/2)
-    return int(head_center[0]//wr), int(head_center[1]//hr)
+    #print("Head Coordinates: ({}, {})".format(int((head_center[0]+1)//wr) +1 , int((head_center[1]+1)//hr)+1))
+    return int((head_center[0]+1)//wr) +1 , int((head_center[1]+1)//hr)+1
+    cv2.imshow('Head Detection', canvas_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def get_apple():
     # Capture screenshot of entire browser window
@@ -116,13 +130,12 @@ def get_apple():
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(canvas_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(canvas_img,"Red Object",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-        print("Red Object Coordinates: ({}, {})".format(int((x+w/2)//wr), int((y+h/2)//hr)))
-    return int((x+w/2)//wr), int((y+h/2)//hr)
-    # # Display the image with the red object detected
-    # cv2.imshow('Red Object Detection', canvas_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+        cv2.putText(canvas_img,"Apple",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+        print("Apple Coordinates: ({}, {})".format(int((x+1)//wr)+1, int((y+1)//hr)+1))
+    return int((x+1)//wr), int((y+1)//hr)
+        # cv2.imshow('Apple Detection', canvas_img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
 class Spot:
     def __init__(self, x, y):
@@ -134,14 +147,18 @@ class Spot:
         self.neighbors = []
         self.camefrom = []
 
+    def show(self, color):
+        draw.rect(screen, color, [self.x*hr+2, self.y*wr+2, hr-4, wr-4])
+
+
     def add_neighbors(self):
-        if self.x > 0:
+        if self.x >= 1:
             self.neighbors.append(grid[self.x - 1][self.y])
-        if self.y > 0:
+        if self.y >= 1:
             self.neighbors.append(grid[self.x][self.y - 1])
-        if self.x < rows - 1:
+        if self.x <= rows - 2:
             self.neighbors.append(grid[self.x + 1][self.y])
-        if self.y < cols - 1:
+        if self.y <= cols - 2:
             self.neighbors.append(grid[self.x][self.y + 1])
 
 grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
@@ -154,22 +171,22 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://www.google.com/fbx?fbx=snake_arcade')
 driver.maximize_window()
 HTML = driver.find_element(By.TAG_NAME, 'html')
-#time.sleep(1)
-HTML.send_keys(Keys.SPACE)
-HTML.send_keys(Keys.ARROW_RIGHT)
-#time.sleep(0.15)
+HTML.send_keys(Keys.SPACE) # inicializar el juego
+HTML.send_keys(Keys.ARROW_RIGHT) # la serpiente se empieza a mover
 canvas = driver.find_element(By.CSS_SELECTOR, '.cer0Bd')
 
-#snake = [grid[9][4],grid[9][5],grid[9][6]]
+#snake = [grid[4][9],grid[5][9],grid[6][9]]
 snake = [grid[6][9]]
-food = grid[14][9]
-current = snake[-1]
+food = grid[14][9] # crea una nueva comida una vez que ya se la comió
+current = snake[-1] # cabeza de la serpiente
 dir_array = get_path(food, snake)
 food_array = [food]
 
 while not done:
-    clock.tick(7.19)
-    direction = dir_array.pop() #devuelve el último valir de la lista y lo elimina
+    clock.tick(6.8)
+    screen.fill(BLACK)
+
+    direction = dir_array.pop(-1) #devuelve el último valir de la lista y lo elimina
     if direction == 0:    # down
         snake.append(grid[current.x][current.y + 1])
         HTML.send_keys(Keys.ARROW_DOWN)
@@ -182,21 +199,33 @@ while not done:
     elif direction == 3:  # left
         HTML.send_keys(Keys.ARROW_LEFT)
         snake.append(grid[current.x - 1][current.y])
-    current = snake[-1] # cabeza de la serpiente
 
-    print(current.x, current.y, food.x, food.y)
+    current = snake[-1] # cabeza de la serpiente
     if current.x == food.x and current.y == food.y:
+        #break
         food_cor = get_apple()
         food = grid[food_cor[0]][food_cor[1]] # crea una nueva comida una vez que ya se la comió
-        # head_cor = get_head()
-        # print(head_cor)
-        # current.x, current.y = head_cor
         food_array.append(food)
         dir_array = get_path(food, snake)
     else:
         snake.pop(0)# elimina la cola
 
-# # Display the image with the red object detected
-# cv2.imshow('Red Object Detection', canvas_img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    for spot in snake:
+        spot.show(WHITE)
+
+
+    food.show(GREEN)
+    snake[-1].show(BLUE)
+    display.flip()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            done = True
+        elif event.type == KEYDOWN:
+            if event.key == K_w and not direction == 0:
+                direction = 2
+            elif event.key == K_a and not direction == 1:
+                direction = 3
+            elif event.key == K_s and not direction == 2:
+                direction = 0
+            elif event.key == K_d and not direction == 3:
+                direction = 1
