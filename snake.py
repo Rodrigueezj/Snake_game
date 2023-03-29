355164f12dc1f44cdabe38c2be4d28bd401a7bf9
Juan <jdbelloro@unal.edu.co>
2023-03-29 17:32:05 -0500

    manzana

--- snake.py
+++ snake.py
@@ -20,10 +20,10 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 done = False
-cols = 17
-rows = 19
-width = 600
-height = 530
+cols = 15
+rows = 17
+width = 600 - (28 * 2)
+height = 530 - (25 * 2)
 wr = width/rows
 hr = height/cols
 direction = 1
@@ -38,9 +38,9 @@ def get_path(food1, snake1):
     openset = [snake1[-1]]
     closedset = []
     dir_array1 = []
-    while 1:
-        current1 = min(openset, key=lambda x: x.f)
-        openset = [openset[i] for i in range(len(openset)) if not openset[i] == current1]
+    while True:
+        current1 = min(openset, key=lambda spot: spot.f)
+        openset = [openset[i] for i in range(len(openset)) if openset[i] != current1]
         closedset.append(current1)
         for neighbor in current1.neighbors:
             if neighbor not in closedset and neighbor not in snake1:
@@ -54,7 +54,7 @@ def get_path(food1, snake1):
                 neighbor.h = sqrt((neighbor.x - food1.x) ** 2 + (neighbor.y - food1.y) ** 2)
                 neighbor.f = neighbor.g + neighbor.h
                 neighbor.camefrom = current1
-        if current1 == food1:
+        if current1 == food1: #Esto se podría poner encuma del fot de la linea 45
             break
     while current1.camefrom:
         if current1.x == current1.camefrom.x and current1.y < current1.camefrom.y:
@@ -103,7 +103,7 @@ def get_head():
     distance = np.sqrt((eye2_center[0] - eye1_center[0])**2 + (eye2_center[1] - eye1_center[1])**2)
     head_center = int((eye1_center[0] + eye2_center[0])/2), int((eye1_center[1] + eye2_center[1])/2)
     #print("Head Coordinates: ({}, {})".format(int((head_center[0]+1)//wr) +1 , int((head_center[1]+1)//hr)+1))
-    return int((head_center[0]+1)//wr) +1 , int((head_center[1]+1)//hr)+1
+    return int((head_center[0]+1)//wr), int((head_center[1]+1)//hr)
     cv2.imshow('Head Detection', canvas_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -113,7 +113,7 @@ def get_apple():
     screenshot_png = driver.get_screenshot_as_png()
     # Convert PNG data to NumPy array
     screenshot_array = np.frombuffer(screenshot_png, dtype=np.uint8)
-    screenshot_img = cv2.imdecode(screenshot_array, cv2.IMREAD_COLOR)
+    screenshot_img = cv2.imdecode(screenshot_array, cv2.IMREAD_ANYCOLOR)
     # Get location and dimensions of canvas element
     canvas_location = canvas.location
     canvas_size = canvas.size
@@ -131,8 +131,8 @@ def get_apple():
         x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(canvas_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(canvas_img,"Apple",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-        print("Apple Coordinates: ({}, {})".format(int((x+1)//wr)+1, int((y+1)//hr)+1))
-    return int((x+1)//wr), int((y+1)//hr)
+        print("Apple Coordinates: ({}, {})".format(int((x - 28) // 32), int((y - 25) // 32)))
+    return int((x - 28) // 32), int((y - 25) // 32)
         # cv2.imshow('Apple Detection', canvas_img)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
@@ -150,15 +150,14 @@ class Spot:
     def show(self, color):
         draw.rect(screen, color, [self.x*hr+2, self.y*wr+2, hr-4, wr-4])
 
-
     def add_neighbors(self):
-        if self.x >= 1:
+        if self.x > 0:
             self.neighbors.append(grid[self.x - 1][self.y])
-        if self.y >= 1:
+        if self.y > 0:
             self.neighbors.append(grid[self.x][self.y - 1])
-        if self.x <= rows - 2:
+        if self.x < rows - 1:
             self.neighbors.append(grid[self.x + 1][self.y])
-        if self.y <= cols - 2:
+        if self.y < cols - 1:
             self.neighbors.append(grid[self.x][self.y + 1])
 
 grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
@@ -169,43 +168,54 @@ for i in range(rows):
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://www.google.com/fbx?fbx=snake_arcade')
-driver.maximize_window()
+#driver.maximize_window()
 HTML = driver.find_element(By.TAG_NAME, 'html')
 HTML.send_keys(Keys.SPACE) # inicializar el juego
 HTML.send_keys(Keys.ARROW_RIGHT) # la serpiente se empieza a mover
-canvas = driver.find_element(By.CSS_SELECTOR, '.cer0Bd')
+canvas = driver.find_element(By.CSS_SELECTOR, '.cer0Bd') #.screenshot_as_png() Sacar solo el screenshot del canvas
 
-#snake = [grid[4][9],grid[5][9],grid[6][9]]
-snake = [grid[6][9]]
-food = grid[14][9] # crea una nueva comida una vez que ya se la comió
+snake = [grid[2][7],grid[3][7],grid[4][7]]
+food = grid[12][7] # crea una nueva comida una vez que ya se la comió
 current = snake[-1] # cabeza de la serpiente
 dir_array = get_path(food, snake)
 food_array = [food]
 
-while not done:
-    clock.tick(6.8)
-    screen.fill(BLACK)
+def draw_grid():
+    for x in range(0, width, int(wr)):
+        for y in range(0, height, int(hr)):
+            rect = pygame.Rect(x, y, int(wr), int(hr))
+            pygame.draw.rect(screen, WHITE, rect, 1)
 
-    direction = dir_array.pop(-1) #devuelve el último valir de la lista y lo elimina
+while not done: 
+    clock.tick_busy_loop(6.77) #framerate
+    screen.fill(BLACK)
+    draw_grid()
+    direction = dir_array.pop(-1) #devuelve el último valor de la lista y lo elimina
     if direction == 0:    # down
         snake.append(grid[current.x][current.y + 1])
         HTML.send_keys(Keys.ARROW_DOWN)
+        displacement = [0,-1]
     elif direction == 1:  # right
         snake.append(grid[current.x + 1][current.y])
         HTML.send_keys(Keys.ARROW_RIGHT)
+        displacement = [-1,0]
     elif direction == 2:  # up
         snake.append(grid[current.x][current.y - 1])
         HTML.send_keys(Keys.ARROW_UP)
+        displacement = [0,1]
     elif direction == 3:  # left
         HTML.send_keys(Keys.ARROW_LEFT)
         snake.append(grid[current.x - 1][current.y])
+        displacement = [1,0]
 
     current = snake[-1] # cabeza de la serpiente
+    
     if current.x == food.x and current.y == food.y:
-        #break
+
         food_cor = get_apple()
         food = grid[food_cor[0]][food_cor[1]] # crea una nueva comida una vez que ya se la comió
         food_array.append(food)
+
         dir_array = get_path(food, snake)
     else:
         snake.pop(0)# elimina la cola
@@ -213,19 +223,9 @@ while not done:
     for spot in snake:
         spot.show(WHITE)
 
-
     food.show(GREEN)
     snake[-1].show(BLUE)
     display.flip()
     for event in pygame.event.get():
         if event.type == QUIT:
             done = True
-        elif event.type == KEYDOWN:
-            if event.key == K_w and not direction == 0:
-                direction = 2
-            elif event.key == K_a and not direction == 1:
-                direction = 3
-            elif event.key == K_s and not direction == 2:
-                direction = 0
-            elif event.key == K_d and not direction == 3:
-                direction = 1
