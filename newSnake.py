545a4da12d3fd708f5632776975b421215decf25
Andres <andgonzalezcas@unal.edu.co>
2023-04-16 19:24:11 -0500

    new snake added

--- newSnake.py
+++ newSnake.py
@@ -2,6 +2,7 @@ import time
 import io
 import numpy as np
 import cv2
+import heapq
 from PIL import Image, ImageDraw
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,6 +13,10 @@ from selenium.webdriver.common.keys import Keys
 cols = 15
 rows = 17
 done = False
+
+# Definir la función heurística (en este caso, distancia Manhattan)
+def heuristic(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 class SnakeBoard:
@@ -88,7 +93,7 @@ class SnakeBoard:
                 y_apple = y
 
             draw.rectangle((x_apple, y_apple, x_apple+w, y_apple+h),
-                          outline=(0, 255, 0), width=2)
+                           outline=(0, 255, 0), width=2)
             draw.text((x_apple, y_apple), 'Apple', fill=(0, 0, 255))
 
           # Imprimimos las coordenadas de cada objeto detectado
@@ -99,7 +104,7 @@ class SnakeBoard:
         canvas_img.save("canvas_screenshot_apple.png")
         coords = (int((x_apple-28) // 32), int((y_apple-25) // 32))
         self.board[coords[0]][coords[1]] = 2
-        self.head = coords
+        self.apple = coords
 
     def updateBody(self, canvas_img):
         hsv_img = cv2.cvtColor(np.array(canvas_img), cv2.COLOR_RGB2HSV)
@@ -126,12 +131,60 @@ class SnakeBoard:
                 coords = (int((x_body-28) // 32), int((y_body-25) // 32))
                 self.board[coords[0]][coords[1]] = 3
 
-                draw.rectangle((x_body, y_body, x_body+w, y_body+h), outline=(0, 255, 0), width=2)
+                draw.rectangle((x_body, y_body, x_body+w, y_body+h),
+                               outline=(0, 255, 0), width=2)
                 draw.text((x_body, y_body), 'Body', fill=(0, 0, 255))
 
         # Devolvemos la última coordenada encontrada
         canvas_img.save("canvas_screenshot_body.png")
-        
+
+    # Definir la función que encuentra el mejor camino usando A*
+    def astar_search(self):
+        grid = self.board
+        start = self.head
+        end = self.apple
+
+        # Definir los movimientos posibles (arriba, abajo, izquierda, derecha)
+        neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        # Definir la cola de prioridad para almacenar los nodos por explorar
+        frontier = [(0, start)]
+        # Definir el diccionario de nodos explorados y sus padres
+        explored = {start: None}
+
+        while frontier:
+            # Obtener el nodo con la menor distancia estimada
+            current_cost, current_node = heapq.heappop(frontier)
+
+            # Si llegamos al nodo final, retornar el camino
+            if current_node == end:
+                path = []
+                while current_node in explored:
+                    path.append(current_node)
+                    current_node = explored[current_node]
+                return path[::-1]
+
+            # Explorar los nodos vecinos
+            for neighbor in neighbors:
+                neighbor_node = (
+                    current_node[0] + neighbor[0], current_node[1] + neighbor[1])
+                # Verificar que el nodo vecino esté dentro del tablero
+                if 0 <= neighbor_node[0] < len(grid) and 0 <= neighbor_node[1] < len(grid[0]):
+                    # Verificar que el nodo vecino no sea un obstáculo
+                    if grid[neighbor_node[0]][neighbor_node[1]] == 0:
+                        # Calcular el costo estimado y el costo real de llegar al nodo vecino
+                        new_cost = current_cost + 1
+                        estimated_cost = new_cost + \
+                            heuristic(end, neighbor_node)
+                        # Si el nodo vecino no ha sido explorado, agregarlo a la cola de prioridad
+                        if neighbor_node not in explored or estimated_cost < explored[neighbor_node][0]:
+                            explored[neighbor_node] = (
+                                estimated_cost, current_node)
+                            heapq.heappush(
+                                frontier, (estimated_cost, neighbor_node))
+
+        # Si no se encuentra un camino, retornar None
+        return None
 
 
 # Esta funcion ejecuta chrome con el link de snake arcade e inicia el juego.
@@ -179,9 +232,19 @@ def getCanvaData(driver):
 driver = initChromeLink()
 canvas_img, widthRow, heightRow = getCanvaData(driver)
 
-snakeBodar = SnakeBoard()
-snakeBodar.cleanBoard()
-snakeBodar.updateHead(canvas_img)
-snakeBodar.updateApple(canvas_img)
-snakeBodar.updateBody(canvas_img)
-snakeBodar.printBoard()
+snakeBoard = SnakeBoard()
+snakeBoard.cleanBoard()
+snakeBoard.updateHead(canvas_img)
+snakeBoard.updateApple(canvas_img)
+# snakeBoard.updateBody(canvas_img)
+snakeBoard.printBoard()
+
+# Llamar a la función para encontrar el mejor camino
+path = snakeBoard.astar_search()
+
+print(path)
+
+# Imprimir el camino encontrado
+if path:
+    for node in path:
+        print(node)
